77e6817f4967e63047f383aa1f5c393674c7aec1
Heloíse Silva Santos <santoshs@taginfraestrutura.com.br>
2022-08-23 20:35:54 -0300

    Trabalhando com parâmetros em rotas

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,9 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('courses') //Cria rota /courses
 export class CoursesController {
     @Get('list') // Cria rota /courses/list
     findAll() {
         return 'Listagem de cursos';
+    }
+
+    @Get(':id') // Define o nome do parâmetro que quero pegar e indicar na rota
+    findOne(
+        @Param('id') id: string // Indica que haverá um recebimento de parâmetros e o tipo dele
+    ) {
+        return `Curso #${id}`; // Parâmetro definido no método Get
     }
 }
